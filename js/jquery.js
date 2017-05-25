96d1cfef36e2e3407c414874aa8f6e8d004f905b
pkoushik <pkoushik11@gmail.com>
2017-05-24 23:11:43 -0700

    skillbars

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,14 @@
-jQuery(document).ready(function() {
-    jQuery('.skillbar').each(function() {
-        jQuery(this).find('.skillbar-bar').animate({
-            width: jQuery(this).attr('data-percent')
-        }, 6000);
-    });
+var skillsDiv = jQuery('#skills');
+
+jQuery(window).on('scroll', function() {
+    var winT = jQuery(window).scrollTop(),
+        winH = jQuery(window).height(),
+        skillsT = skillsDiv.offset().top;
+    if (winT + winH > skillsT) {
+        jQuery('.skillbar').each(function() {
+            jQuery(this).find('.skillbar-bar').animate({
+                width: jQuery(this).attr('data-percent')
+            }, 6000);
+        });
+    }
 });
