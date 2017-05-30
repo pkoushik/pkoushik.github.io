eb26c61775f0a883c6a5a2bea6c5d5c39c843a43
pkoushik <pkoushik11@gmail.com>
2017-05-30 16:22:17 -0700

    weekly update

--- js/init.js
+++ js/init.js
@@ -173,6 +173,19 @@ jQuery(document).ready(function($) {
         });
         return false;
     });
+    var skillsDiv = jQuery('#skills');
 
+    jQuery(window).on('scroll', function() {
+        var winT = jQuery(window).scrollTop(),
+            winH = jQuery(window).height(),
+            skillsT = skillsDiv.offset().top;
+        if (winT + winH > skillsT) {
+            jQuery('.skillbar').each(function() {
+                jQuery(this).find('.skillbar-bar').animate({
+                    width: jQuery(this).attr('data-percent')
+                }, 6000);
+            });
+        }
+    });
 
 });
